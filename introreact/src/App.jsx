bd13f199958d84e36de073adbb48bb81df22abac
ISAQUE RIBEIRO DA SILVA <isaque_r_silva@estudante.sesisenai.org.br>
2025-08-05 22:09:54 -0300

    Primeira aula dwe React

---
+++ introreact/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+
+      
+      <h1>pg de react js</h1>
+
+      <MotivosPraUsarReact />
+    </>
+  )
+}
+
+export default App
